cd24025b235bd4741adfbdba96336d171f4b196b
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-06-21 21:21:00 -0300

    feat: Criado rota GET /activities para pesquisar todas as atividades

--- src/routes/activitiesRouter.js
+++ src/routes/activitiesRouter.js
@@ -1,5 +1,21 @@
 const express = require('express');
 
+const {
+  getAllActivities
+} = require('../utils/fsUtils');
+
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const activities = await getAllActivities();
+
+  try {
+    return res.status(200).json(activities);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Um erro inesperado ocorreu no servidor, tente mais tarde!',
+    });
+  };
+});
 
 module.exports = router;
---
+++ src/utils/fsUtils.js
@@ -0,0 +1,28 @@
+const fs = require('fs/promises');
+const { join, resolve } = require('path');
+
+const ACTIVITIES_FILE_PATH = '../activities.json';
+
+const readActivities = async () => {
+    const data = await fs.readFile(join(__dirname, ACTIVITIES_FILE_PATH), 'utf-8');
+
+    if (!data) {
+      return [];
+    }
+    return JSON.parse(data);
+}
+
+const writeActivities = async (data) => {
+  const convertedData = JSON.stringify(data);
+  await fs.writeFile(resolve(__dirname, path), convertedData);
+}
+
+const getAllActivities = async () => {
+  const activities = await readActivities();
+
+  return activities;
+}
+
+module.exports = {
+  getAllActivities,
+}
